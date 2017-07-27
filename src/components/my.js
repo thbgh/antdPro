990e05a8c9d0c50d5a34eb88ee5e83a51dfdb9e7
THB <thbwork2016@gmail.com>
2017-07-27 16:26:18 +0800

    优化

---
+++ src/components/my.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: THB
+ * @Date:   2017-07-26
+ * @Email:  thbwork2016@gmail.com
+ * @Filename: my.js
+ * @Last modified by:   THB
+ * @Last modified time: 2017-07-26 06:26:53
+ */
+ssdfsdfsfd
+dsddd
