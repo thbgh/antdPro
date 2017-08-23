352f79a674877d2356a112ff5ab5fc70e117646a
THB <thbwork2016@gmail.com>
2017-08-23 18:03:06 +0800

    mock数据

--- src/components/button.js
+++ src/components/button.js
@@ -1,16 +1,53 @@
-import React, {Component} from 'react';
-import {Button} from 'antd';
+import React, { Component } from 'react';
+import { Button } from 'antd';
+// process.env.NODE_ENV=='mock' && require('../mock/mock1.js');
+import '../mock/mock1';
+import $ from 'jquery';
 
 export default class Btn extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Button</h3>
-                <Button type="primary">Primary</Button>
-                <Button>Default</Button>
-                <Button type="dashed">Dashed</Button>
-                <Button type="danger">Danger</Button>
-            </div>
-        );
-    }
+  componentDidMount() {
+    /**
+         * 请求1 get 请求
+         */
+    $.ajax({ url: 'https://1.json', type: 'post' }).done(function(
+      data,
+      status
+    ) {
+      console.log(data);
+      // console.log(data.list)
+      // //方式一：使用eval解析
+      // var obj = eval(data)
+      // // console.dir(obj.constructor)
+      // console.dir(obj)
+    });
+
+    // fetch('https://1.json', {
+    //     method: 'post',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         // api_name: "user.show_user_companies",
+    //         // access_token: YBCommon.get_cookie('access_token'),
+    //         // user_id: YBCommon.get_cookie('user_id'), from: "PC"
+    //     })
+    // }).then((response) => response.json()).then((json) => {
+    //     console.log({json: json});
+    // }).catch((error) => {
+    //     console.log(error);
+    //
+    // });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Button</h3>
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
+      </div>
+    );
+  }
 }
